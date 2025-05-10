716f21707dc953bb74bfb1984e3939fa3e524ed3
Animeshti <animesht550@gmail.com>
2025-05-10 22:59:04 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,42 @@
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    if num2 == 0:
+        return "Cannot divide by zero!"
+    return num1 / num2 
+
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("Enter choice(1/2/3/4): ")
+
+num1_str = input("Enter first number: ")
+num2_str = input("Enter second number: ")
+
+try:
+    num1 = float(num1_str)
+    num2 = float(num2_str)
+
+    if choice == '1':
+        print(num1, "+", num2, "=", add(num1, num2))
+    elif choice == '2':
+        print(num1, "-", num2, "=", subtract(num1, num2))
+    elif choice == '3':
+        print(num1, "*", num2, "=", multiply(num1, num2))
+    elif choice == '4':
+        print(num1, "/", num2, "=", divide(num1, num2))
+    else:
+        print("Invalid input")
+
+except ValueError:
+    print("Invalid number input. Please enter numbers only.") 
